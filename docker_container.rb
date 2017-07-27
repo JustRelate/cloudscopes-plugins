e29116be96121761277597c80e585cd5cba78cae
Tilo Prütz <tilo@infopark.de>
2017-07-27 19:49:06 +0200

    [docker_container] configurable path for data volume monitoring

--- docker_container.rb
+++ docker_container.rb
@@ -2,13 +2,14 @@ category "Docker Container"
 
 describe_samples do
   if container_filter = ENV['CS_DOCKER_CONTAINER_NAME_FILTER']
+    fs_path = ENV['CS_DOCKER_CONTAINER_DATA_PATH'] || '/'
     container_ids = docker.ps(name_filter: container_filter)
     instance_id = ec2.instance_id
     container_ids.each do |c_id|
       i_total, i_used, i_avail =
-          *docker.exec(c_id, "df", "-i", "/").split("\n").last.split.map(&:to_i)
+          *docker.exec(c_id, "df", "-i", fs_path).split("\n").last.split.map(&:to_i)
       total, used, avail =
-          *docker.exec(c_id, "df", "-B", "1", "/").split("\n").last.split.map(&:to_i)
+          *docker.exec(c_id, "df", "-B", "1", fs_path).split("\n").last.split.map(&:to_i)
 
       aggregation_dimensions = {}
       if aggregation_group = ENV['CS_AGGREGATION_GROUP']
