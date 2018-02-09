0bf4d4d5b1a59b376c533dee47631b8be2ebdde8
Tilo Prütz <tilo@infopark.de>
2018-02-09 21:23:51 +0100

    [resque] improved metrics; partially high resolution

--- resque.rb
+++ resque.rb
@@ -3,20 +3,32 @@ require 'resque-scheduler'
 
 category "Resque"
 
+sample_interval 10
+
 Resque.redis = Redis.new
 
 describe_samples do
-  info = ::Resque.info
+  cluster = ENV['ECS_CLUSTER']
   queue_sizes = ::Resque.queue_sizes
-  delayed = ::Resque.delayed_queue_schedule_size
+  queue_name_prefix = "#{cluster.rpartition('-').last}_"
+  queue_sizes.select! {|k, _| k.start_with?(queue_name_prefix) }
+  queue_names = queue_sizes.keys
+  pending = queue_sizes.values.reduce(&:+) || 0
+  workers = ::Resque.workers.select {|w| queue_names.any? {|name| w.queues.include?(name) } }
 
-  sample(name: "Working", unit: "Count", value: info[:working])
-  sample(name: "Pending", unit: "Count", value: info[:pending])
-  sample(name: "Workers", unit: "Count", value: info[:workers])
+  delayed = ::Resque.find_delayed_selection do |args|
+    args.any? {|arg| Hash === arg && queue_names.include?(arg['queue_name']) }
+  end.count
 
-  queue_sizes.each do |name, size|
-    sample(dimensions: {QueueName: name}, name: "Size", unit: "Count", value: size)
+  opts = {aggregate: {}, dimensions: {ClusterName: cluster}}
+
+  sample(**opts, name: "Working", unit: "Count", value: workers.count(&:working?))
+  sample(**opts, name: "Workers", unit: "Count", value: workers.count)
+  sample(**opts, name: "Pending", unit: "Count", value: pending, storage_resolution: 1)
+  if workers.count > 0
+    sample(**opts, name: "Pending per Worker", unit: "Count", value: pending.to_f / workers.count,
+        storage_resolution: 1)
   end
 
-  sample(name: "Delayed", unit: "Count", value: delayed)
+  sample(**opts, name: "Delayed", unit: "Count", value: delayed)
 end
