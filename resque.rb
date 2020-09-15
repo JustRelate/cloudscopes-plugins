e5de1218265952ce3a8d991e90d15ddc16fce01a
Tilo Prütz <tilo@infopark.de>
2020-09-15 13:41:42 +0200

    report resque metrics per queue(s) (plus RuboCop compliance)

--- resque.rb
+++ resque.rb
@@ -10,29 +10,57 @@ Resque.redis = Redis.new
 describe_samples do
   queue_sizes = ::Resque.queue_sizes
   queue_name_prefix = "#{ec2.ecs_cluster.rpartition('-').last}_"
-  queue_sizes.select! {|k, _| k.start_with?(queue_name_prefix) }
-  pending = queue_sizes.values.reduce(&:+) || 0
-  workers = ::Resque.workers.select {|w| w.queues.any? {|q| q.start_with?(queue_name_prefix)} }
+  pending_per_queue = queue_sizes
+    .select {|k, _| k.start_with?(queue_name_prefix) }
+    .transform_keys {|k| k[queue_name_prefix.size..-1] }
+  pending = pending_per_queue.values.reduce(&:+) || 0
+  workers = ::Resque.workers.select {|w| w.queues.any? {|q| q.start_with?(queue_name_prefix) } }
 
   ::Resque::Worker.all_workers_with_expired_heartbeats.each do |worker|
     next unless workers.include?(worker)
 
     # prune code copied from ::Resque::Worker#prune_dead_workers which does too many other things
-    job_class = worker.job(false)['payload']['class'] rescue nil
+    job_class = worker.job(false)&.[]('payload')&.[]('class')
     worker.unregister_worker(::Resque::PruneDeadWorkerDirtyExit.new(worker.to_s, job_class))
     workers -= [worker]
   end
-  working = workers.count(&:working?)
+  working_per_queues = Hash.new(0)
+  working = 0
+  workers.select(&:working?).each do |w|
+    key = w.queues.map {|n| n[queue_name_prefix.size..-1] }.sort.join(", ")
+    working_per_queues[key] += 1
+    working += 1
+  end
 
-  delayed = ::Resque.find_delayed_selection do |args|
-    args.any? {|arg| Hash === arg && arg['queue_name'].start_with?(queue_name_prefix) }
-  end.count
+  delayed_per_queue = Hash.new(0)
+  delayed = 0
+  ::Resque.find_delayed_selection do |args|
+    args.any? do |arg|
+      if arg.is_a?(Hash)
+        queue = arg['queue_name']
+        if queue.start_with?(queue_name_prefix)
+          delayed_per_queue[queue[queue_name_prefix.size..-1]] += 1
+          delayed += 1
+          true
+        end
+      end
+    end
+  end
 
   opts = {aggregate: {}, dimensions: {ClusterName: ec2.ecs_cluster}}
 
-  sample(**opts, name: "Working", unit: "Count", value: working, storage_resolution: 1)
+  working_per_queues.each do |k, v|
+    sample(**opts, name: "Working #{k}", unit: "Count", value: v, storage_resolution: 1)
+  end
   sample(**opts, name: "Working low-res", unit: "Count", value: working)
-  sample(**opts, name: "Pending", unit: "Count", value: pending, storage_resolution: 1)
+
+  pending_per_queue.each do |k, v|
+    sample(**opts, name: "Pending #{k}", unit: "Count", value: v, storage_resolution: 1)
+  end
   sample(**opts, name: "Pending low-res", unit: "Count", value: pending)
+
+  delayed_per_queue.each do |k, v|
+    sample(**opts, name: "Delayed #{k}", unit: "Count", value: v)
+  end
   sample(**opts, name: "Delayed", unit: "Count", value: delayed)
 end
