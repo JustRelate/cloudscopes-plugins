595418a74f21df5096887c55e94f8d90b1dc6565
Tilo Prütz <tilo@infopark.de>
2020-09-15 13:40:58 +0200

    add Rubocop config and make docker_nginx.rb compliant

--- docker_nginx.rb
+++ docker_nginx.rb
@@ -11,40 +11,36 @@ describe_samples do
   request_time = Time.now
 
   response = Net::HTTP.get_response(URI("http://127.0.0.1/nginx_status"))
-  unless response.code == "200"
-    raise "Failed to get Nginx status: #{response.code} #{response.body}"
-  end
+  raise "Failed to get Nginx status: #{response.code} #{response.body}" unless response.code == "200"
 
   response.body.split("\n").each do |line|
-    total = $1.to_i if line =~ /^Active connections:\s+(\d+)/
+    total = Regexp.last_match(1).to_i if line =~ /^Active connections:\s+(\d+)/
     if line =~ /^Reading:\s+(\d+).*Writing:\s+(\d+).*Waiting:\s+(\d+)/
-      reading = $1.to_i
-      writing = $2.to_i
-      waiting = $3.to_i
+      reading = Regexp.last_match(1).to_i
+      writing = Regexp.last_match(2).to_i
+      waiting = Regexp.last_match(3).to_i
     end
-    requests = $3.to_i if line =~ /^\s+(\d+)\s+(\d+)\s+(\d+)/
+    requests = Regexp.last_match(3).to_i if line =~ /^\s+(\d+)\s+(\d+)\s+(\d+)/
   end
 
-  if last_requests
-    requests_per_second = (requests - last_requests) / (request_time - last_request_time)
-  end
+  requests_per_second = (requests - last_requests) / (request_time - last_request_time) if last_requests
   self.last_requests = requests
   self.last_request_time = request_time
 
   rack_active = rack_queued = 0
   `cat /proc/net/unix`.split("\n").each do |line|
-    if line =~ %r[/(unicorn|puma).sock$]
-      _, _, _, _, _, _, inode, _ = line.split(' ')
-      if inode == "0"
-        rack_queued += 1
-      else
-        rack_active += 1
-      end
+    next unless line =~ %r{/(unicorn|puma).sock$}
+
+    _, _, _, _, _, _, inode, = line.split(' ')
+    if inode == "0"
+      rack_queued += 1
+    else
+      rack_active += 1
     end
   end
 
   aggregation_dimensions = {}
-  if aggregation_group = ENV['CS_AGGREGATION_GROUP']
+  if (aggregation_group = ENV['CS_AGGREGATION_GROUP'])
     aggregation_dimensions[:group] = aggregation_group
   end
   opts = {
@@ -59,8 +55,6 @@ describe_samples do
   sample(**opts, name: "Request Throughput", unit: "Count/Second", value: requests_per_second)
 
   opts[:dimensions] = {ClusterName: ec2.ecs_cluster}
-  sample(**opts, name: "Active Rack Connections", unit: "Count", value: rack_active,
-      storage_resolution: 1)
-  sample(**opts, name: "Queued Rack Connections", unit: "Count", value: rack_queued,
-      storage_resolution: 1)
+  sample(**opts, name: "Active Rack Connections", unit: "Count", value: rack_active, storage_resolution: 1)
+  sample(**opts, name: "Queued Rack Connections", unit: "Count", value: rack_queued, storage_resolution: 1)
 end
